3d5503afd991fdb3b2f05d685f90794a4338e4aa
CoolizzLuo <duenzo1010@gmail.com>
2021-04-21 00:34:23 +0800

    w2 hw1~6 complete

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,27 @@
 function join(arr, concatStr) {
-  
+    let result = ''
+    for(let i = 0; i < arr.length; i++) {
+        if (i === arr.length -1) {
+            result += arr[i]
+        } else {
+            result += arr[i] + concatStr;
+        }
+    }
+    return result
 }
 
 function repeat(str, times) {
-  
+    let result = '';
+    for(let i = 1; i <= times; i++) {
+        result += str;
+    }
+    return result;
 }
 
 console.log(join(['a'], '!'));
+console.log(join([''], '!'));
+console.log(join([1, 2, 3], ''));
+console.log(join(["a", "b", "c"], "!"));
+console.log(join(["a", 1, "b", 2, "c", 3], ','));
 console.log(repeat('a', 5));
+console.log(repeat('yoyo', 2));
