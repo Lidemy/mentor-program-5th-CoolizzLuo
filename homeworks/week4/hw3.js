8fe8db9031674a7061d2d54d4b1237f7023843af
Enzo <enzo.luo@neurv.com>
2021-05-10 12:32:39 +0800

    fix week4 hw

---
+++ homeworks/week4/hw3.js
@@ -0,0 +1,18 @@
+const API = 'https://restcountries.eu/rest/v2/name'
+const request = require('request')
+
+request.get(`${API}/tai`, (err, res, body) => {
+  if (err) return console.log('catch error', err)
+  if (res.statusCode >= 200 && res.statusCode < 300) {
+    const data = JSON.parse(body)
+    data.forEach(renderCountry)
+  }
+})
+
+function renderCountry(country) {
+  console.log('============')
+  console.log(`國家:${country.name}`)
+  console.log(`首都:${country.capital}`)
+  console.log(`貨幣:${country.callingCodes[0]}`)
+  console.log(`國碼:${country.currencies[0].code}`)
+}
